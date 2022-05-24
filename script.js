9c62d291761368854f3e0391d7b6696186af0a1d
mohammad6vakili <mohammad6vakili@gmail.com>
2022-05-24 22:46:56 +0430

    all landing exept footer last section

--- script.js
+++ script.js
@@ -3,8 +3,26 @@ const swiper = new Swiper('.swiper', {
   spaceBetween: 30,
   slidesPerGroup: 4,
   loopFillGroupWithBlank: true,
+  autoplay: {
+    delay: 2000,
+  },
   pagination: {
     el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+const techSwiper = new Swiper('.techSwiper', {
+  slidesPerView: 5,
+  spaceBetween: 30,
+  slidesPerGroup: 5,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination-tech",
     clickable: true,
   },
   navigation: {
