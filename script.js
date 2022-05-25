3b3f6a09d13ff0657180a8471aa05b71e4d2fd74
mohammad6vakili <mohammad6vakili@gmail.com>
2022-05-25 12:00:02 +0430

    landing finish

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper(".swiper", {
   slidesPerView: 4,
   spaceBetween: 30,
   slidesPerGroup: 4,
@@ -14,13 +14,45 @@ const swiper = new Swiper('.swiper', {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+  breakpoints: {
+    1200: {
+      slidesPerView: 4,
+      spaceBetweenSlides: 30,
+    },
+    800: {
+      slidesPerView: 2,
+      spaceBetweenSlides: 40,
+    },
+    320: {
+      slidesPerView: 1,
+      spaceBetweenSlides: 40,
+    },
+  },
 });
 
-const techSwiper = new Swiper('.techSwiper', {
+const techSwiper = new Swiper(".techSwiper", {
   slidesPerView: 5,
   spaceBetween: 30,
   slidesPerGroup: 5,
   loopFillGroupWithBlank: true,
+  breakpoints: {
+    1200: {
+      slidesPerView: 5,
+      spaceBetweenSlides: 30,
+    },
+    800: {
+      slidesPerView: 3,
+      spaceBetweenSlides: 40,
+    },
+    400: {
+      slidesPerView: 2,
+      spaceBetweenSlides: 40,
+    },
+    320: {
+      slidesPerView: 1,
+      spaceBetweenSlides: 40,
+    },
+  },
   pagination: {
     el: ".swiper-pagination-tech",
     clickable: true,
@@ -29,4 +61,20 @@ const techSwiper = new Swiper('.techSwiper', {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+});
+
+document.getElementById("backToTop").addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
+
+document.getElementById("hamButton").addEventListener("click", () => {
+  document.getElementById("hamMenu").classList.add("hamOpen");
+});
+
+document.getElementById("main").addEventListener("click", () => {
+  document.getElementById("hamMenu").classList.remove("hamOpen");
+});
+
+document.getElementById("footer").addEventListener("click", () => {
+  document.getElementById("hamMenu").classList.remove("hamOpen");
 });
